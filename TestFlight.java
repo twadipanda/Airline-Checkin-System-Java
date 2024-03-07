8f0e427e1d2151a9750841594726bdc6d34f66b3
abdulgiz <73882431+abdulgiz@users.noreply.github.com>
2024-03-07 09:46:42 +0000

    added in invalid booking and test flight

---
+++ TestFlight.java
@@ -0,0 +1,52 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import app.Controller;
+import app.Data;
+import app.Flight;
+import exceptions.*;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * unit test for the Flight class
+ */
+public class TestFlight {
+ 
+	/*
+	 * tests whether the Data read methods throws a FileNotFoundException
+	 * when provided with a file that does not exist
+	*/
+	@Test
+	public void assertThrowsFileNotFoundException() {
+		assertThrows(FileNotFoundException.class, () -> {
+		Data data = new Data("etc.csv", "etc.csv");
+		data.read();
+    });
+	}
+
+	/*
+	 * tests whether the Data read methods throws a InvalidBookingReference
+	 * when provided with an invalid booking
+	*/
+	@Test
+	public void assertThrowsInvalidBookingReference() {
+		assertThrows(InvalidBookingReference.class, () -> {
+		Data data = new Data("src/invalid_bookings.csv", "src/flights.csv");
+		data.read();
+		});
+	}
+
+	/*
+	 * tests whether the Data read methods returns a Controller object
+	*/
+	@Test
+	public void assertReturnTypeIsController() throws FileNotFoundException, InvalidBookingReference {
+		Data data = new Data("src/bookings.csv", "src/flights.csv");
+		assertEquals(data.read().getClass(), Controller.class);
+	}
+}
